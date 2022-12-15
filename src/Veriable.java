fcb20ae26a83948a90ead317747112c880515ee9
YuYoHan <zxzz8014@naver.com>
2022-12-15 10:59:24 +0900

    [Study] Veriable

---
+++ src/Veriable.java
@@ -0,0 +1,11 @@
+public class Veriable {
+    public static void main(String[] args) {
+        // 변수선언
+        int num;
+        // 변수 초괴화
+        num =2 ;
+        System.out.println(num);
+
+
+    }
+}
