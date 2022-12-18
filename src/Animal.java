238f085d381fd1d8a31d3764a731d9ecdfdc1bec
YuYoHan <zxzz8014@naver.com>
2022-12-18 13:09:37 +0900

    [Study] class

---
+++ src/Animal.java
@@ -0,0 +1,19 @@
+public class Animal {
+    String name;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // 메소드
+    public void sound() {
+        System.out.println("~~~");
+    }
+}
+
+
